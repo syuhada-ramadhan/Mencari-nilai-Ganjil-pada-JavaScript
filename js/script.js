9f516a6a9a2328afc07caec6c9a4388bfb2bdb55
syuhada-ramadhan <suhadahada18@gmail.com>
2021-01-11 20:12:09 +0700

    Latihan Mencari nilai Ganjil pada JavaScript selesai

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// array nomor
+const nomor = [1, 4, 5, 8, 10, 30, 50, 100];
+
+// cari nomor ganjil menggunakan method filter
+const ganjil = nomor.filter((no) => no % 2 === 1);
+
+// 5 % 2 === 1;
+// 2 * 2 = 4;
+// 5 - 4 = 1;
+
+// cetak variabel ganjil
+console.log(ganjil);
